df6dee2abe749ffd65dda8c16c0d54dda7abc375
Andrew Willis <arwillis@uncc.edu>
2021-08-30 11:54:59 -0400

    Added license headers.

--- src/cpuBackProjection/cpuBackProjection.hpp
+++ src/cpuBackProjection/cpuBackProjection.hpp
@@ -1,10 +1,19 @@
-/* 
- * File:   gpuBackProjectionKernel.cuh
- * Author: arwillis
+/*
+ * Copyright (C) 2021 Andrew R. Willis
  *
- * Created on June 12, 2021, 10:29 AM
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #ifndef CPUBACKPROJECTION_HPP
 #define CPUBACKPROJECTION_HPP
 
